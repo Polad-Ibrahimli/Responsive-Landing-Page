833fc41f8196de648f8288a04178cab333c3bfc8
Polad <poladibrahimli1@gmail.com>
2026-07-21 01:45:22 +0400

    Javascipt added for form validations

--- index.js
+++ index.js
@@ -2,7 +2,32 @@ const menuToggle = document.querySelector('.menu-toggle');
 const navElements = document.querySelector('.nav-elements');
 const button=document.querySelector(".contact-form button");
 
-button.addEventListener("click",event=>event.preventDefault())
+button.addEventListener("click",event=>{
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const namePattern=/^[a-zA-Z]+ [a-zA-Z]/;
+
+    const nameValue=document.getElementById("fname").value;
+    const emailValue=document.getElementById("femail").value;
+
+    const nameError=document.getElementById("nameError");
+    const emailError=document.getElementById("emailError");
+
+    event.preventDefault();
+    if (!emailPattern.test(emailValue)) {
+     emailError.textContent = "Please enter a valid email address!";
+    }
+    else{
+        document.getElementById("emailError").textContent="";
+    }
+    if(!namePattern.test(nameValue)){
+        nameError.textContent="Please enter a valid name!";
+    }
+    else{
+        nameError.textContent="";
+    }
+
+})
 
 menuToggle.addEventListener('click', () => {
     navElements.classList.toggle('active');
