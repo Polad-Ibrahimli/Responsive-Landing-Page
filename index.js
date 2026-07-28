872fad06026ee9d77832a06fec43c094f6f55690
Polad <poladibrahimli1@gmail.com>
2026-07-29 01:26:44 +0400

    final updates are made

--- index.js
+++ index.js
@@ -1,40 +1,46 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const navElements = document.querySelector('.nav-elements');
-const button=document.querySelector(".contact-form button");
 
-button.addEventListener("click",event=>{
-
+const menuToggle = document.querySelector('.header__toggle');
+const navList = document.querySelector('.header__nav-list');
+const submitButton = document.querySelector('.contact__submit');
+ 
+submitButton.addEventListener("click", event => {
+ 
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const namePattern=/^[a-zA-Z]+ [a-zA-Z]/;
-
-    const nameValue=document.getElementById("fname").value;
-    const emailValue=document.getElementById("femail").value;
-    const additionalMsg=document.getElementById("fnotes").value;
-
-    const nameError=document.getElementById("nameError");
-    const emailError=document.getElementById("emailError");
-    const notesError=document.getElementById("notesError");
-
+    const namePattern = /^[a-zA-Z]+ [a-zA-Z]+$/;
+ 
+    const nameValue = document.getElementById("fname").value;
+    const emailValue = document.getElementById("femail").value;
+    const additionalMsg = document.getElementById("fnotes").value;
+ 
+    const nameError = document.getElementById("nameError");
+    const emailError = document.getElementById("emailError");
+    const notesError = document.getElementById("notesError");
+ 
     event.preventDefault();
+ 
     if (!emailPattern.test(emailValue)) {
-     emailError.textContent = "Please enter a valid email address!";
+        emailError.textContent = "Please enter a valid email address!";
+    } else {
+        emailError.textContent = "";
     }
-    else{
-        document.getElementById("emailError").textContent="";
+ 
+    if (!namePattern.test(nameValue)) {
+        nameError.textContent = "Please enter a valid name!";
+    } else {
+        nameError.textContent = "";
     }
-    if(!namePattern.test(nameValue)){
-        nameError.textContent="Please enter a valid name!";
+ 
+    if (additionalMsg === "") {
+        notesError.textContent = "This part should not be empty.";
+    } else {
+        notesError.textContent = "";
     }
-    else{
-        nameError.textContent="";
-    }
-    if(additionalMsg===""){
-        notesError.textContent="This part should not be empty."
-    }
-
+ 
 })
-
+ 
 menuToggle.addEventListener('click', () => {
-    navElements.classList.toggle('active');
+    navList.classList.toggle('active');
+    const isExpanded = navList.classList.contains("active");
+    menuToggle.setAttribute("aria-expanded", isExpanded);
 });
 
